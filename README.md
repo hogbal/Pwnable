ba3878177e82138d0f4cd2bc817b0e6a50a6f8ca
hogbal <hogbal31@gmail.com>
2020-07-21 09:08:09 -0700

    pwnable.tw calc

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ simple login 하는중
 
 <h2>pwnable.tw</h2>
 CVE-2018-1160 하는중
+calc 하는중
---
+++ pwnable.tw/calc/calc.py
@@ -0,0 +1,6 @@
+from pwn import *
+context.log_level='debug'
+
+n = remote('chall.pwnable.tw',10100)
+
+n.interacvite()
