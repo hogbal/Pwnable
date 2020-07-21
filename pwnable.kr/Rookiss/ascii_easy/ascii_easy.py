b4afcd3df386cc05dc9b3f70ff2ad8d13a73f04a
hogbal <hogbal31@gmail.com>
2020-07-21 11:52:17 -0700

    ascii_easy.py

---
+++ pwnable.kr/Rookiss/ascii_easy/ascii_easy.py
@@ -0,0 +1,13 @@
+from pwn import *
+#context.log_level='debug'
+
+s = ssh('ascii_easy','pwnable.kr',port=2222,password='guest')
+
+argvs=[str(i) for i in range(2)]
+
+payload = 'AAAA'
+
+argvs[1] = payload
+p = s.process(executable='./ascii_easy',argv=argvs)
+
+p.interactive()
